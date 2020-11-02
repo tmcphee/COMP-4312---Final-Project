8396e424b7032353289583f256d9fa457cf7112c
tyler <32398415+tmcphee@users.noreply.github.com>
2020-11-02 12:44:23 -0500

    Updated DockerSetup.bat Docker added tag and push base.html added sticky nav

--- DockerSetup.bat
+++ DockerSetup.bat
@@ -1,6 +1,8 @@
 cd /D "%~dp0"
-docker run python:3.8
+
 docker build -t hotelreview .
+docker tag hotelreview tmcphee/hotelreview
+docker push tmcphee/hotelreview:latest
 docker run -d -p 8080:8080 hotelreview
 docker ps
 
