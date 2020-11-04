b32b9e9e34e3476dd6a7211aac3f37b945293fd1
fapfapdragon <tdgames29@gmail.com>
2020-11-04 18:22:57 -0500

    Picked best ML model

---
+++ data_analysing.py
@@ -0,0 +1,111 @@
+import os
+import subprocess
+
+import pandas as pd
+import numpy as np
+from wordcloud import WordCloud
+import ntlk
+import matplotlib.pyplot as plt
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+import nltk
+nltk.download('stopwords')
+
+
+def main():
+    df = get_data("Dataset/hotel-reviews.csv")
+    #describe_data(df)
+    #print("Training data converted to lower case: ")
+    #data_to_lower(df)
+    #describe_data(df)
+    #convert_words(df)
+    #df.to_csv(r'Dataset/hotel-reviews-cleansed.csv', index = False)
+    split_data(df)
+    return
+
+def split_data(df):
+    train, test = train_test_split(df, test_size=0.3) #70, 30 train test split
+    describe_data(train)
+    describe_data(test)
+    test.to_csv(r'Dataset/test.csv', index=False)
+    train.to_csv(r'Dataset/train.csv', index=False)
+    return
+
+def convert_words(df):
+    dataset = df
+    eng_stopwords = set(stopwords.words("english"))
+    sub_text = dataset[dataset.Is_Response == "not happy"].Description.values
+    non_text = dataset[dataset.Is_Response == "happy"].Description.values
+    from collections import Counter
+    c_sub = Counter()
+    for sent in sub_text:
+        c_sub.update(sent.split())
+    c_non = Counter()
+    for sent in non_text:
+        c_non.update(sent.split())
+    inter_words = set(c_non).intersection(set(c_sub))
+    inter_words.difference_update(eng_stopwords)
+    print("Common words use in both: %d" % (len(inter_words)))
+    inter_words
+    sus_wd = []
+    for wd in inter_words:
+        if c_sub[wd] >= c_non[wd] * 3:
+            sus_wd.append(wd)
+    print(sus_wd)
+
+def describe_data(df):
+    l_train = len(df)
+    print(df.head(10))
+    print(df.info())
+    print(l_train)
+    print("Label distribution of training data:")
+    print((df.Is_Response.value_counts() / l_train))
+    return
+
+def data_to_lower(df):
+    df.Description = df.Description.str.lower()
+    return
+
+def impute_data(df):
+    print("Null check on training data:")
+    null_check = df.isna().sum()
+    if (null_check):
+        print("Null Values Found")
+    return df
+
+def plot_data(df):
+    # matplotlib inline
+    dataset = df
+    eng_stopwords = set(stopwords.words("english"))
+
+    plt.figure(figsize=(20, 20))
+    plt.subplot(121)
+    sub_text = dataset[dataset.Is_Response == "happy"].Description.values
+    subwc = WordCloud(background_color="black", max_words=2000, stopwords=eng_stopwords)
+    subwc.generate(" ".join(sub_text))
+    plt.axis("off")
+    plt.title("Words frequented of happy reviews", fontsize=20)
+    plt.imshow(subwc.recolor(colormap='gist_earth', random_state=244), alpha=0.98)
+    plt.subplot(122)
+    non_text = dataset[dataset.Is_Response == "not happy"].Description.values
+    nonwc = WordCloud(background_color="black", max_words=2000, stopwords=eng_stopwords)
+    nonwc.generate(" ".join(non_text))
+    plt.axis("off")
+    plt.title("Words frequented of unhappy reviews", fontsize=20)
+    plt.imshow(nonwc.recolor(colormap='Reds', random_state=244), alpha=0.98)
+    plt.savefig('Dataset/word_cloud.png', bbox_inches='tight', dpi=600)
+    return
+
+def get_data(name):
+    #Get the data, from local csv
+    if os.path.exists(name):
+        print(name + " found locally")
+        df = pd.read_csv(name)
+
+        return df
+    else:
+        print("File does not exist")
+        return 0
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,199 @@
+import os
+import time
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_roc_curve
+
+from sklearn.naive_bayes import MultinomialNB  # NB
+from sklearn.neighbors import KNeighborsClassifier  # k-NN
+from sklearn.linear_model import SGDClassifier  # logistic regression
+from sklearn.tree import DecisionTreeClassifier  # DT
+from sklearn.svm import LinearSVC  # linear SVM
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
+
+
+def pkl_write(data, filename='data.pickle'):
+    with open(filename, 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+
+def pkl_read(filename='data.pickle'):
+    with open(filename, 'rb') as f:
+        # The protocol version used is detected automatically, so we do not
+        # have to specify it.
+        data = pickle.load(f)
+    return data
+
+
+def build_cls(ml_cls="KNN", tfidf=False, use_hash=False, scaler=False):
+    print("- Construct the baseline...")
+    start = time.time()
+    if ml_cls == "kNN":
+        classifier = KNeighborsClassifier(n_neighbors=5)
+    elif ml_cls == "LR":
+        # Logistic Regression
+        classifier = SGDClassifier(verbose=5, loss='log', max_iter=100)
+    elif ml_cls == "DT":
+        classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+    elif ml_cls == "SVM":
+        classifier = LinearSVC(verbose=5)
+    elif ml_cls == "MLP":
+        classifier = MLPClassifier(random_state=1, max_iter=100)
+    elif ml_cls == "AB":
+        classifier = AdaBoostClassifier()
+    elif ml_cls == "GB":
+        classifier = GradientBoostingClassifier(verbose=5)
+    elif ml_cls == "RF":
+        classifier = RandomForestClassifier(n_estimators=100, verbose=5)
+    else:
+        # DEFAULT: NB
+        classifier = MultinomialNB()
+    settings = []
+    if use_hash:
+        settings = [('vectorizer', HashingVectorizer())]
+    elif tfidf:
+        settings = [('vectorizer', TfidfVectorizer())]
+    else:
+        # DEFAULT: BOW counting
+        settings = [('vectorizer', CountVectorizer())]
+
+    # scaler cannot use with NB (MultinomialNB)
+    if scaler and ml_cls != "NB":
+        settings += [('scaler', StandardScaler())]
+
+    settings += [('classifier', classifier)]
+    model = Pipeline(settings)
+
+    parameters = {'vectorizer__analyzer': ['word'],
+                  # 'vectorizer__analyzer': ['char', 'word'],
+                  'vectorizer__ngram_range': [(1, 5)],
+                  'vectorizer__min_df': [3, 5, 7],
+                  'vectorizer__binary': (True, False)
+                  }
+    end = time.time()
+    print("\t+ Done: %.4f(s)" % (end - start))
+    return model, parameters
+
+
+def train(args):
+    data_train = pd.read_csv(args.train_file).sample(frac=1).reset_index(drop=True)
+    # data_dev = pd.read_csv(args.dev_file).sample(frac=1).reset_index(drop=True)
+    # data_merge = pd.concat([data_train, data_dev])
+    # dev_fold = [-1]*len(data_train) + [0]*len(data_dev)
+    # x_traindev, y_traindev = data_merge["text"].to_numpy(), data_merge["label"].to_numpy()
+
+    x_traindev, y_traindev = data_train["Description"].to_numpy(), data_train["Is_Response"].to_numpy()
+    pipeline, parameters = build_cls(args.ml_cls, args.tfidf, args.use_hash, args.scaler)
+
+    print("- Train the baseline...")
+    start = time.time()
+    # model = GridSearchCV(pipeline, parameters, cv=PredefinedSplit(test_fold=dev_fold),
+    #                      verbose=5,  scoring='f1_weighted')
+    model = GridSearchCV(pipeline, parameters, cv=5, verbose=5, scoring='f1_weighted')
+    model.fit(x_traindev, y_traindev)
+    end = time.time()
+    print("\t+ Done: %.4f(s)" % (end - start))
+    best_model = model.best_estimator_
+    save(best_model, args.model_name)
+    return model
+
+
+def save(model, mfile):
+    print("- Save the model...")
+    pkl_write(model, mfile)
+    print("\t+ Done.")
+
+
+def load(mfile):
+    print("- Load the model...")
+    model = pkl_read(mfile)
+    print("\t+ Done.")
+    return model
+
+
+def evaluate(data, model_name):
+    model = load(model_name)
+    print("- Evaluate the baseline...")
+    start = time.time()
+    X_dev, y_true = data
+    y_pred = model.predict(X_dev)
+    mtrcs = class_metrics(y_true, y_pred)
+    plot_confusion_matrix(model, X_dev, y_true)
+    plot_roc_curve(model, X_dev, y_true)
+    end = time.time()
+    print("\t+ Done: %.4f(s)" % (end - start))
+    return mtrcs
+
+
+def test(args, model_name):
+    data_test = pd.read_csv(args.test_file).sample(frac=1).reset_index(drop=True)
+    x_test, y_test = data_test["Description"].to_numpy(), data_test["Is_Response"].to_numpy()
+    mtrcs = evaluate([x_test, y_test], model_name)
+    return mtrcs
+
+
+def class_metrics(y_true, y_pred):
+    acc = metrics.accuracy_score(y_true, y_pred)
+    f1_ma = metrics.precision_recall_fscore_support(y_true, y_pred, average='macro')
+    f1_we = metrics.precision_recall_fscore_support(y_true, y_pred, average='weighted')
+    f1_no = metrics.precision_recall_fscore_support(y_true, y_pred, average=None)
+    print("\t+ Accuracy: %.4f(%%)" % (acc * 100))
+    measures = {"acc": acc, "prf_macro": f1_ma, "prf_weighted": f1_we, "prf_individual": f1_no}
+    return measures
+
+
+def predict(sent, model_name):
+    model = load(model_name)
+    label = model.predict([sent]).tolist()[0]
+    prob = model.predict_proba([sent]).max()
+    print("- Inference...")
+    print("\t+ %s with p=%.4f" % (label, prob))
+    return label, prob
+
+
+if __name__ == '__main__':
+    """
+    python baselines.py --train_file /media/data/langID/small_scale/train.csv --dev_file /media/data/langID/small_scale/dev.csv --test_file /media/data/langID/small_scale/test.csv --model_name ./results/small.NB.m --ml_cls NB
+    """
+    argparser = argparse.ArgumentParser(sys.argv[0])
+
+    argparser.add_argument('--train_file', help='Trained file', default="Dataset/train.csv", type=str)
+
+    argparser.add_argument('--dev_file', help='Developed file', default="Dataset/test.csv", type=str)
+
+    argparser.add_argument('--test_file', help='Tested file', default="Dataset/test.csv", type=str)
+
+    argparser.add_argument("--tfidf", action='store_true', default=False, help="tfidf flag")
+
+    argparser.add_argument("--use_hash", action='store_true', default=False, help="hashing flag")
+
+    argparser.add_argument("--scaler", action='store_true', default=False, help="scale flag")
+
+    argparser.add_argument('--ml_cls', help='Machine learning classifier', default="KNN", type=str)
+
+    argparser.add_argument('--model_dir', help='Model dir', default="Dataset", type=str)
+
+    args = argparser.parse_args()
+
+    model_dir, _ = os.path.split(args.model_dir)
+
+    if not os.path.exists(args.model_dir):
+        os.mkdir(args.model_dir)
+    args.model_name = os.path.join(args.model_dir, args.ml_cls + ".pickle")
+
+    #model = train(args)
+    measures = test(args, args.model_name)
+    # label, prob = predict("call us to win a price", args.model_name)
