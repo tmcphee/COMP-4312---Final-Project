59c2207408749d6e8665a9b8fd5bfc059b94db6d
tyler <32398415+tmcphee@users.noreply.github.com>
2020-11-19 12:35:24 -0500

    Implemented SQL Redial when connection is lost

--- SQL.py
+++ SQL.py
@@ -2,22 +2,70 @@ import pymysql.cursors
 import subprocess
 import os
 import threading
+import signal
 from settings import *
 
 #cloud_sql_proxy -instances=<INSTANCE_NAME>>=tcp:3306 -credential_file=credentials.json &
 connection = None
 proxy_started = False
+proxy_instance = None
+SQL_INITIAL_CONNECT = False
 
 
 def sql_connect():
-    global connection
+    global connection, SQL_INITIAL_CONNECT
     try:
         connection = pymysql.connect(host=SQL_HOST,
                                      user=SQL_USER,
                                      password=SQL_PASSWORD,
                                      db=SQL_DB)
+        SQL_INITIAL_CONNECT = True
     except Exception as e:
         print("SQL CONNECTION ERROR - Exeception occured:{}".format(e))
+
+
+def startproxy():
+    global proxy_instance
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    if ENVIRONMENT == "Windows":
+        proxypath = os.path.join(ROOT_DIR, "cloud_sql_proxy.exe")
+        proxy_instance = subprocess.Popen(proxypath + " -instances=" + INSTANCE_NAME + "=tcp:3306 -credential_file=" + GOOGLE_APPLICATION_CREDENTIALS + " &")
+        print("Using Windows style SQL Proxy")
+    else:
+        proxypath = os.path.join(ROOT_DIR, "cloud_sql_proxy")
+        proxy_instance = subprocess.Popen(proxypath + " -instances=" + INSTANCE_NAME + "=tcp:3306 -credential_file=" + GOOGLE_APPLICATION_CREDENTIALS + " &",
+                         shell=True)
+        proxy_instance
+        print("Using Linux style SQL Proxy")
+
+
+def sql_proxy_run():
+    try:
+        startproxy()
+        sql_connect()
+    except Exception as e:
+        print("SQL PROXY RUN ERROR - Exeception occured:{}".format(e))
+
+
+# Redial connection to SQL if connection is lost
+# Dont try to reconnect if connection is initially lost
+def redial_sql():
+    try:
+        if SQL_INITIAL_CONNECT == False:
+            return False
+        if proxy_instance != None:
+            print("KILLING OLD CONNECTION TO SQL PROXY...")
+            try:
+                os.kill(os.getpgid(proxy_instance.pid), signal.SIGTERM)
+            except Exception as e:
+                print(e)
+        print("REDIALING CONNECTION TO SQL PROXY...")
+        sql_proxy_run()
+        return True
+    except:
+        print("CANNOT CONNECT TO SQL DB")
+        return False
 
 
 def sql_insert(query):
@@ -25,7 +73,7 @@ def sql_insert(query):
         cursor = connection.cursor()
         cursor.execute(query)
     except Exception as e:
-        print("SQL ERROR - Exeception occured:{}".format(e))
+        print("SQL insert ERROR - Exeception occured:{}".format(e))
 
 
 def sql_select(query):
@@ -34,7 +82,11 @@ def sql_select(query):
         cursor.execute(query)
         return cursor.fetchall()
     except Exception as e:
-        print("SQL ERROR - Exeception occured:{}".format(e))
+        print("SQL select ERROR - Exeception occured:{}".format(e))
+        if redial_sql() == True and connection is not None:
+            cursor = connection.cursor()
+            cursor.execute(query)
+            return cursor.fetchall()
         return [["NA", "**MySQL DB OFFLINE*", "NA"]]
 
 
@@ -43,43 +95,7 @@ def sql_format_response(content):
     return data
 
 
-def startproxy():
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    if ENVIRONMENT == "Windows":
-        proxypath = os.path.join(ROOT_DIR, "cloud_sql_proxy.exe")
-        subprocess.Popen(proxypath + " -instances=" + INSTANCE_NAME + "=tcp:3306 -credential_file=" + GOOGLE_APPLICATION_CREDENTIALS + " &")
-        print("Using Windows style SQL Proxy")
-    else:
-        proxypath = os.path.join(ROOT_DIR, "cloud_sql_proxy")
-        subprocess.Popen(proxypath + " -instances=" + INSTANCE_NAME + "=tcp:3306 -credential_file=" + GOOGLE_APPLICATION_CREDENTIALS + " &",
-                         shell=True)
-        print("Using Linux style SQL Proxy")
-
-
-def sql_proxy_run():
-    try:
-        '''t = threading.Thread(target=startproxy())
-        t.daemon = True
-        t.start()'''
-        startproxy()
-
-        sql_connect()
-        '''
-        retrycount = 3
-        if str(connection) is None:
-            sql_connect()
-            if str(connection) is not None:
-                return
-            while retrycount != 0:
-                print("SQL DB not found. Retrying Connection .. ")
-                sql_connect()
-                if str(connection) is not None:
-                    break
-                os.wait(10)
-                retrycount -= 1'''
-    except Exception as e:
-        print("SQL PROXY RUN ERROR - Exeception occured:{}".format(e))
 
 sql_proxy_run()
 #sql_insert("DROP TABLE Reviews")
